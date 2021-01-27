dc2e935f7aac4f4eae7e6fe847cb7a93c7a8651c
iliana <iliana.marcano@walmart.com>
2021-01-27 12:36:12 -0300

    Fix shell to math correct argument for gocheckcov

--- run-gocheckcov.sh
+++ run-gocheckcov.sh
@@ -3,12 +3,12 @@ FILES=$(go list ./...  | grep -v /vendor/)
 
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
-    echo "Please add `args: [-min-coverage=60]` in your pre-commit config"
+    echo "Please add `args: [--minimum-coverage=60]` in your pre-commit config"
     exit 1
 fi
 
 
-gocheckcov check --minimum-coverage $@  ${FILES}
+gocheckcov check "$@"  ${FILES}
 
 returncode=$?
 if [ $returncode -ne 0 ]; then
